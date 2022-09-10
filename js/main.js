ea8e368dc274d54caf118506216aa1dc53c43850
Javier <yard.campbs@gmail.com>
2022-09-10 16:14:58 +0100

    Add currency rates API

--- js/main.js
+++ js/main.js
@@ -1,42 +1,64 @@
+import { addHours } from "./utility.js";
+
 const buttons = Array.from(document.getElementsByClassName("calc-button"));
 let topScreen = document.getElementById("operations");
 
-function add(n1, n2){
-    return n1 + n2;
+function updateRates(json, currentDate) {
+  // Obtain current conversion rates
+  fetch(json)
+    .then((response) => response.json())
+    .then((data) => {
+      // Convert rates to local data
+      localStorage.setItem("currency", JSON.stringify(data));
+    });
+  // Rates won't be updated until next 24 hours
+  localStorage.setItem("updateTime", addHours(currentDate, 24));
 }
 
-function substract(){
-    return n1 - n2;
+function add(n1, n2) {
+  return n1 + n2;
 }
 
-function multiply(){
-    return n1 * n2;
+function substract() {
+  return n1 - n2;
 }
 
-function divide(){
-    return n1 / n2;
+function multiply() {
+  return n1 * n2;
 }
 
-function operate(n1, n2, operator){
-    switch(operator){
-        case "+": 
-            add(n1, n2)
-            break;
-        case "-":
-            substract(n1, n2);
-            break;
-        case "*":
-            multiply(n1, n2);
-            break;
-        case "/":
-            divide(n1, n2);
-            break;
-    }
+function divide() {
+  return n1 / n2;
 }
 
+function operate(n1, n2, operator) {
+  switch (operator) {
+    case "+":
+      add(n1, n2);
+      break;
+    case "-":
+      substract(n1, n2);
+      break;
+    case "*":
+      multiply(n1, n2);
+      break;
+    case "/":
+      divide(n1, n2);
+      break;
+  }
+}
 
-buttons.forEach(e => {
-    e.addEventListener("click", () => {
-        topScreen.innerText += e.innerText;
-    })
-})
+window.onload = () => {
+  if (
+    !localStorage.getItem("updateTime") ||
+    new Date() >= new Date(localStorage.getItem("updateTime"))
+  ) {
+    updateRates("https://api.exchangerate-api.com/v4/latest/euro", new Date());
+  }
+};
+
+buttons.forEach((e) => {
+  e.addEventListener("click", () => {
+    topScreen.innerText += e.innerText;
+  });
+});
