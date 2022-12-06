50df1a727746b04c733eb3167770f970bf74fb5c
yarduon <yard.campbs@gmail.com>
2022-12-06 21:38:59 +0000

    Align screen texts mobile

--- workbox-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  globDirectory: "./",
-  globPatterns: ["**/*.{css,ttf,yml,png,svg,html,js,json,md}"],
-  swDest: "sw.js",
-  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-};
